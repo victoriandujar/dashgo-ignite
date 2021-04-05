439510c6d9551cf778f37c55d6d260e45ba97893
Victoria <victoria.andujar10@gmail.com>
2021-04-05 15:04:24 -0300

    Component Input

--- .history/src/pages/index_20210405150320.js
+++ .history/src/pages/index_20210405150320.js
@@ -1,6 +1,10 @@
-import { Flex, Button, Stack } from "@chakra-ui/react";
+import {
+  Flex,
+  Button,
+  Stack
+} from "@chakra-ui/react";
 
-import { Input } from "../components/Form/input";
+import {Input} from '../components/Form/input';
 
 export default function Home() {
   return (
@@ -15,8 +19,8 @@ export default function Home() {
         flexDir="column"
       >
         <Stack spacing="4">
-          <Input type="email" name="email" label="E-mail" />
-          <Input type="password" name="password" label="Senha" />
+          <Input type="email" name="email" label="E-mail"/>
+          <Input type="password" name="password" label="Senha"/>
         </Stack>
 
         <Button type="submit" mt="6" colorScheme="pink" size="lg">
