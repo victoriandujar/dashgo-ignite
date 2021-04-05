fc1486d6189b7403736dc6297beab532de1d9117
Victoria <victoria.andujar10@gmail.com>
2021-04-05 14:19:10 -0300

    Config Chakra

--- .history/src/styles/theme_20210405141719.ts
+++ .history/src/styles/theme_20210405141719.ts
@@ -16,8 +16,8 @@ export const theme = extendTheme({
     },
   },
   fonts: {
-    heading: "Roboto",
-    body: "Roboto",
+    heading: 'Roboto',
+    body: 'Roboto',
   },
   styles: {
     global: {
