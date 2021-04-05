419fc51a731147c26b809a4cf521b3a40a9b68e1
Victoria <victoria.andujar10@gmail.com>
2021-04-05 18:53:15 -0300

    User Form

---
+++ .history/src/pages/users/create_20210405184107.tsx
@@ -0,0 +1,17 @@
+import { Box, Flex } from "@chakra-ui/react";
+
+import { Header } from "../../components/Header";
+import { Sidebar } from "../../components/Sidebar";
+
+export default function UserList() {
+  return (
+    <Box>
+      <Header />
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="20">
+        <Sidebar />
+
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8"></Box>
+      </Flex>
+    </Box>
+  );
+}
