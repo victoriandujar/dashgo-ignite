fcc26c7ff2e58fdc082c3d830a6d6c4f47e76fde
Victoria <victoria.andujar10@gmail.com>
2021-04-05 15:27:02 -0300

    Creating header

---
+++ .history/src/components/Header_20210405151259.tsx
@@ -0,0 +1,18 @@
+import { Flex } from "@chakra-ui/react";
+
+export function Header() {
+  return (
+    <Flex
+      as="header"
+      w="100%"
+      maxWidth={1480}
+      h="20"
+      mx="auto"
+      mt="4"
+      px="6"
+      align="center"
+    >
+      <h1>ahuasha</h1>
+    </Flex>
+  );
+}
---
+++ .history/src/pages/index_20210405150605.tsx
@@ -0,0 +1,28 @@
+import { Flex, Button, Stack } from "@chakra-ui/react";
+
+import { Input } from "../components/Form/input";
+
+export default function Home() {
+  return (
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <Input type="email" name="email" label="E-mail" />
+          <Input type="password" name="password" label="Senha" />
+        </Stack>
+
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
+}
