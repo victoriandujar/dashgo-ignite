c5656962ae68c59af7b18b5b6f61efe0c9f70dad
Victoria <victoria.andujar10@gmail.com>
2021-04-05 18:39:07 -0300

    Pagination Component

---
+++ .history/src/components/Pagination_20210405183550.tsx
@@ -0,0 +1,44 @@
+import { Stack, Button } from "@chakra-ui/react";
+
+export function Pagination() {
+  return (
+    <Stack
+      direction="row"
+      mt="8"
+      justify="space-between"
+      spacing="6"
+      align="center"
+    >
+      <Button
+        size="sm"
+        fontSize="xs"
+        width="4"
+        colorScheme="pink"
+        disabled
+        _disabled={{ bgColor: "pink.500", cursor: "default" }}
+      >
+        1
+      </Button>
+
+      <Button
+        size="sm"
+        fontSize="xs"
+        width="4"
+        bg="gray.700"
+        _hover={{ bg: "gray.500" }}
+      >
+        2
+      </Button>
+
+      <Button
+        size="sm"
+        fontSize="xs"
+        width="4"
+        bg="gray.700"
+        _hover={{ bg: "gray.500" }}
+      >
+        3
+      </Button>
+    </Stack>
+  );
+}
