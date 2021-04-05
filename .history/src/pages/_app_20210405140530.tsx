8d2b9efaf303580bcd29847061b80338650f93e9
Victoria <victoria.andujar10@gmail.com>
2021-04-05 14:11:20 -0300

    Config Chakra UI

--- .history/src/pages/_app_20210405140530.tsx
+++ .history/src/pages/_app_20210405140530.tsx
@@ -4,7 +4,7 @@ import { theme } from "../styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider resetCSS theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
---
+++ .history/src/styles/theme_20210405140119.ts
@@ -0,0 +1,12 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const styles = extendTheme({
+  styles: {
+    global: {
+      body: {
+        bg: "gray.900",
+        color: "gray.50",
+      },
+    },
+  },
+});
