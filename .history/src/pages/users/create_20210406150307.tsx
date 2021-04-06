8882067812f937f018eacd6e5ef49327b7f0d06f
Victoria <victoria.andujar10@gmail.com>
2021-04-06 15:03:58 -0300

    Responsive Form

--- .history/src/pages/users/create_20210406150307.tsx
+++ .history/src/pages/users/create_20210406150307.tsx
@@ -8,7 +8,7 @@ export default function UserList() {
   return (
     <Box>
       <Header />
-      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="20">
         <Sidebar />
 
         <Box flex="1" borderRadius={8} bg="gray.800" p={["6", "8"]}>
