902f9ebf2a0944027b435a698a4d63986a3a2688
Victoria <victoria.andujar10@gmail.com>
2021-04-13 16:36:34 -0300

    Pagination Component

---
+++ .history/src/components/Pagination/Pagination_20210413162155.tsx
@@ -0,0 +1,59 @@
+import { Stack, Box } from "@chakra-ui/react";
+import { PaginationItem } from "./PaginationItem";
+
+interface PaginationProps {
+  totalCountOfRegisters: number;
+  registersPerPage?: number;
+  currentPage?: number;
+  onPageChange: (page: number) => void;
+}
+
+const siblingsCount = 1;
+
+function generatePagesArray(from: number, to: number) {
+  return [...new Array(to - from)]
+  .map((_, index) => {
+    return from + index + 1;
+  })
+  .filter(page => page > 0);
+}
+
+export function Pagination({
+  totalCountOfRegisters,
+  registersPerPage = 10,
+  currentPage = 1,
+  onPageChange,
+}: PaginationProps) {
+  const lastPage = Math.floor(totalCountOfRegisters / registersPerPage);
+
+  const previousPage = currentPage > 1
+  ? generatePagesArray(currentPage - 1 - siblingsCount, currentPage - 1)
+  : []
+
+  const nextPages = currentPage < lastPage
+  ? generatePagesArray(currentPage, Math.min(currentPage + siblingsCount, lastPage))
+  : []
+  
+  return (
+    <Stack
+      direction={["column", "row"]}
+      mt="8"
+      justify="space-between"
+      spacing="6"
+      align="center"
+    >
+      <Box>
+        <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
+      </Box>
+
+      <Stack direction="row" spacing="2">
+        <PaginationItem number={1} isCurrent />
+        <PaginationItem number={2} />
+        <PaginationItem number={3} />
+        <PaginationItem number={4} />
+        <PaginationItem number={5} />
+        <PaginationItem number={6} />
+      </Stack>
+    </Stack>
+  );
+}
