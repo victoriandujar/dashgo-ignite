09a87d70a9880d5f0e941610399f9c3e93661abd
Victoria <victoria.andujar10@gmail.com>
2021-04-13 15:15:42 -0300

    Creating list hook

---
+++ .history/src/services/hooks/useUsers_20210413151200.ts
@@ -0,0 +1,27 @@
+import { useQuery } from "react-query";
+import { api } from "../api";
+
+async function getUsers() {
+  const { data } = await api.get("/users");
+
+  const users = data.users.map((user) => {
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      }),
+    };
+  });
+
+  return users;
+}
+
+export function useUsers() {
+  return useQuery("users", getUsers, {
+    staleTime: 1000 * 5,
+  });
+}
