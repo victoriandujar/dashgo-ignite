11d01475a892331f188963c942b791fb2d9ae5af
Victoria <victoria.andujar10@gmail.com>
2021-04-13 14:55:37 -0300

    Signaling data refetch

--- .history/src/pages/users/index_20210413145435.tsx
+++ .history/src/pages/users/index_20210413145435.tsx
@@ -62,7 +62,7 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
-              { !isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4"/>}
+              { !isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4"/>
             </Heading>
             <Link href="/users/create" passHref>
               <Button
