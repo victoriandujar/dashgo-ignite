110b8ec5d489df33b3e09635ea0d8dd680d7c7eb
Victoria <victoria.andujar10@gmail.com>
2021-04-13 15:27:16 -0300

    Pagination Logic

--- .history/src/services/mirage/index_20210413145915.ts
+++ .history/src/services/mirage/index_20210413145915.ts
@@ -1,4 +1,4 @@
-import { createServer, Model, Factory, Response } from "miragejs";
+import { createServer, Model, Factory } from "miragejs";
 
 import faker from "faker";
 
@@ -29,29 +29,14 @@ export function makeServer() {
     },
 
     seeds(server) {
-      server.createList("user", 200);
+      server.createList("user", 10);
     },
 
     routes() {
       this.namespace = "api";
       this.timing = 750;
 
-      this.get("/users", function (schema, request) {
-        const { page = 1, per_page = 10 } = request.queryParams;
-        const total = schema.all("user").length;
-
-        const pageStart = (Number(page) - 1) * Number(per_page);
-
-        const pageEnd = pageStart + Number(per_page);
-
-        const users = this.serialize(schema.all("user")).users.slice(
-          pageStart,
-          pageEnd
-        );
-
-        return new Response(200, { "x-total-count": String(total) }, { users });
-      });
-
+      this.get("/users");
       this.post("/users");
 
       this.namespace = "";
