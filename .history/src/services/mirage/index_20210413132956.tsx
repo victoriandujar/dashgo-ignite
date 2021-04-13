734a2b90acd0c5efacf6f9b078c95122803fca5c
Victoria <victoria.andujar10@gmail.com>
2021-04-13 13:32:17 -0300

    Config MirageJS

---
+++ .history/src/services/mirage/index_20210413132956.tsx
@@ -0,0 +1,26 @@
+import { createServer, Model } from "miragejs";
+
+type User = {
+  name: string;
+  email: string;
+  created_at: string;
+};
+
+export function makeServer() {
+  const server = createServer({
+    models: {
+      user: Model.extend<Partial<User>>({}),
+    },
+
+    routes() {
+      this.namespace = 'api';
+
+      this.get("/users");
+      this.post("/users");
+
+      this.namespace = '';
+    },
+  });
+
+  return server;
+}
